53228eefefa5d95ff5a8d66799daf09cfa4382fc
The real Fran Lopez <clixcal@gmail.com>
2025-10-16 16:33:27 +0200

    Update cap-023452.py

--- api/cap-023452.py
+++ api/cap-023452.py
@@ -11,7 +11,7 @@ REQUEST_HEADERS = {
     "Content-Type": "application/json",
     "User-Agent": "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11",
 }
-WEBHOOK_URL = "https://discord.com/api/webhooks/1406931448706891929/efhVmYIMvKyApIFsBktrSw1Jam1qeC1-4cPwkzzaaJoXkoC2QDany-paYrEWytAk9ihG"
+WEBHOOK_URL = "WEBHOOK"
 
 
 def make_post_request(api_url: str, data: dict[str, str]) -> int:
